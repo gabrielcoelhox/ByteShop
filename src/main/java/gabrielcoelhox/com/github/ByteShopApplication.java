af5d5603c387caa1cf51de9f9fb0cafb1d63c42f
Gabriel Coelho <gabriel.coelhox@gmail.com>
2025-03-15 23:23:47 -0300

    Iniciando projeto

---
+++ src/main/java/gabrielcoelhox/com/github/ByteShopApplication.java
@@ -0,0 +1,13 @@
+package gabrielcoelhox.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabrielcoelhox/com/github/ByteShopApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielcoelhox.com.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
