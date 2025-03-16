0202a7dcfd771ab928c5dd68b05bb9a1771b8739
Gabriel Coelho <gabriel.coelhox@gmail.com>
2025-03-16 00:21:16 -0300

    Add base para iniciar autenticacao

---
+++ src/main/java/gabrielcoelhox/com/github/dto/auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package gabrielcoelhox.com.github.dto.auth;
+
+import gabrielcoelhox.com.github.dto.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private UserDTO user;
+}
---
+++ src/main/java/gabrielcoelhox/com/github/dto/auth/RegisterRequest.java
@@ -0,0 +1,31 @@
+package gabrielcoelhox.com.github.dto.auth;
+
+import gabrielcoelhox.com.github.model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 25, message = "Username must be between 3 and 25 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    private UserRole role = UserRole.USER;
+}
